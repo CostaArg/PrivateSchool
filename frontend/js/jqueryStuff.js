9a568374a340e3f0ad69d75d413764fb568b0bc0
Konstantinos Argyropoulos <konstantinos.argyropoulos@Peoplecert.onmicrosoft.com>
2020-02-19 00:56:49 +0200

    fixed comments

--- frontend/js/jqueryStuff.js
+++ frontend/js/jqueryStuff.js
@@ -1,16 +1,9 @@
-const script = document.createElement("script"); // Create a <script> element
-script.src = "https://code.jquery.com/jquery-3.4.1.min.js"; // Add a src attribute
-document.head.appendChild( script ); // Append it to <head>
-// When the script loads, and jQuery has been loaded, you are ready to go:
- 
-script.onload = function(){
- 
- console.log("Loaded!");
- 
- jQuery(function($){
- 
- $("#menu").load("./menu.html");
- 
- });
- 
-}
+const script = document.createElement("script");
+script.src = "https://code.jquery.com/jquery-3.4.1.min.js";
+document.head.appendChild(script);
+
+script.onload = function() {
+  jQuery(function($) {
+    $("#menu").load("./menu.html");
+  });
+};
